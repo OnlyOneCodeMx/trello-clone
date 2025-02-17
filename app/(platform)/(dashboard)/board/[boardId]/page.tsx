3a44c9c57633827c4afb8a9ea5e1628279db5d08
Miguel Angel Diaz <101842830+OnlyOneCodeMx@users.noreply.github.com>
2025-02-17 12:46:02 -0600

    feat(list): implement board page and list container
    - Add `page.tsx` to fetch and display lists for a board
    - Create `ListContainer` component to manage and render lists

---
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -0,0 +1,40 @@
+import { auth } from '@clerk/nextjs/server';
+
+import { db } from '@/lib/db';
+import { ListContainer } from './_components/list-container';
+
+interface BoardIdPageProps {
+  params: { boardId: string };
+}
+
+export const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { boardId } = await params;
+  const { orgId } = await auth();
+
+  const list = await db.list.findMany({
+    where: {
+      boardId: boardId,
+      board: {
+        orgId: orgId!,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: 'asc',
+        },
+      },
+    },
+    orderBy: {
+      order: 'asc',
+    },
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={boardId} data={list} />
+    </div>
+  );
+};
+
+export default BoardIdPage;
